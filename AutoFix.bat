3755c2426bce26b1275e930a7564bfed5cf390fd
CrypticDay <121768693+CrypticDay@users.noreply.github.com>
2023-09-21 21:34:39 +0700

    Update AutoFix.bat

--- AutoFix.bat
+++ AutoFix.bat
@@ -1,7 +1,7 @@
 @echo off
 
-rem ถามผู้ใช้ว่า RPGM เป็นประเภทไหน
-echo โปรดเลือกประเภทของ RPGM:
+rem asks the user what type of RPGM it is.
+echo Please select the type of RPGM:
 echo 1. RPG Maker MV
 echo 2. RPG Maker VX Ace
 echo 3. RPG Maker VX
@@ -9,49 +9,49 @@ echo 4. RPG Maker XP
 
 set /p type=
 
-rem ตรวจสอบประเภทของ RPGM
+rem Check the types of RPGM
 if "%type%" == "1" (
 
-rem โหลด RPG Maker MV
-echo กำลังดาวน์โหลด RPG Maker MV...
+rem Download RPG Maker MV
+echo Downloading RPG Maker MV...
 curl -o RPG-Maker-MV.zip https://dl.komodo.jp/rpgmakerweb/trial-download/RPGMV_161W_Setup.zip
 
-rem เปิด RPG Maker MV
-echo กำลังเปิด RPG Maker MV...
+rem Open RPG Maker MV
+echo Opening RPG Maker MV...
 start /wait RPG-Maker-MV.zip
 
 ) else if "%type%" == "2" (
 
-rem โหลด RPG Maker VX Ace
-echo กำลังดาวน์โหลด RPG Maker VX Ace...
+rem Download RPG Maker VX Ace
+echo Downloading RPG Maker VX Ace...
 curl -o RPG-Maker-VX-Ace.exe https://dl.komodo.jp/rpgmakerweb/trial-download/RPGVXAce_Multi.exe
 
-rem เปิด RPG Maker VX Ace
-echo กำลังเปิด RPG Maker VX Ace...
+rem Open RPG Maker VX Ace
+echo Opening RPG Maker VX Ace...
 start /wait RPG-Maker-VX-Ace.exe
 
 ) else if "%type%" == "3" (
 
-rem โหลด RPG Maker VX
-echo กำลังดาวน์โหลด RPG Maker VX...
+rem Download RPG Maker VX
+echo Downloading RPG Maker VX...
 curl -o RPG-Maker-VX.exe https://dl.komodo.jp/rpgmakerweb/trial-download/RPGVX_E.exe
 
-rem เปิด RPG Maker VX
-echo กำลังเปิด RPG Maker VX...
+rem Open RPG Maker VX
+echo Opening RPG Maker VX...
 start /wait RPG-Maker-VX.exe
 
 ) else if "%type%" == "4" (
 
-rem โหลด RPG Maker XP
-echo กำลังดาวน์โหลด RPG Maker XP...
+rem Download RPG Maker XP
+echo Downloading RPG Maker XP...
 curl -o RPG-Maker-XP.exe https://dl.komodo.jp/rpgmakerweb/trial-download/RPGXP_E.exe
 
-rem เปิด RPG Maker XP
-echo กำลังเปิด RPG Maker XP...
+rem Open RPG Maker XP
+echo Opening RPG Maker XP...
 start /wait RPG-Maker-XP.exe
 
 ) else (
 
-echo ตัวเลือกไม่ถูกต้อง
+echo Invalid choice.
 
 )
